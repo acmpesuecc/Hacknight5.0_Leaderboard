e16233578879d0ca99b5ff59bb7809ddc6bef084
anuragrao04 <raoanu2004@gmail.com>
2023-09-17 15:59:48 +0530

    set font family to use jetbrains. Setup background a little. only font logic left

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,14 @@
   import '../app.css'
 </script>
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
+
 <slot />
 
+<style>
+  *{
+    font-family: 'JetBrains Mono', monospace;
+  }
+</style>
--- src/routes/background.svelte
+++ src/routes/background.svelte
@@ -2,3 +2,17 @@
 <main>
   <div>background test</div>
 </main>
+
+
+<style>
+  div{
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: #0F0913;
+    z-index: 0;
+    color: #655F67;
+  }
+</style>
