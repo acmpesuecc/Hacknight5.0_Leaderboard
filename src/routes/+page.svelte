2456c3b7a607993482cefa0598837e65375da171
anuragrao04 <raoanu2004@gmail.com>
2023-09-18 13:33:24 +0530

    added cards on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,32 @@
   import Card from './Card.svelte';
   export let data;
   console.log(data.json_response);
+
+
+  let leaderboard = [
+    {
+    name: "cupcake",
+    score: 209,
+    },
+    {
+    name: "anuragrao04",
+    score: 121,
+    },
+    {
+    name: "VyoJ",
+    score: 0,
+    },
+  ];
+
+
 </script>
 
 <main>
   <Background />
+  {#each leaderboard as person, i}
+    <Card index={i} user={person.username}, points={person.score}/>
+  {/each}
 
+  
   
 </main>
