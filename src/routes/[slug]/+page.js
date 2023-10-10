88c7aecddc416fa4e642d9ccbf355de09277e4d2
anuragrao04 <raoanu2004@gmail.com>
2023-10-10 19:25:38 +0530

    missed a /

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -4,7 +4,7 @@ export async function load({ params, fetch }) {
   let username = params.slug;
   try {
     const response = await fetch(
-      `https:/hacknight.navinshrinivas.com/records?user=${username}`
+      `https://hacknight.navinshrinivas.com/records?user=${username}`
     );
     if (!response.ok) {
       throw new Error(response.status, "Reddy Anna Is Not Talking");
